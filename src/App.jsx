f5b7f597b7ba8dbbe624cef158efcf26cadcf117
PRINCE SINGH RAO <princerao1235u@gmail.com>
2026-02-22 13:07:50 +0530

    Correct import path for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 import Products from "./pages/Products";
 import Services from "./pages/Services";
 import About from "./pages/About";
